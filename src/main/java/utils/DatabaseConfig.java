dcf70c511aad5902be4188b8919f8f8ef34ee19a
ZZG0003 <ZZG0003@my.londonmet.ac.uk>
2026-04-21 09:07:17 +0100

    Update DatabaseConfig.java
    Debug path updated to actual path

--- src/main/java/utils/DatabaseConfig.java
+++ src/main/java/utils/DatabaseConfig.java
@@ -1,5 +1,6 @@
 package utils;
 
+import db.DatabaseSetup;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,12 +8,17 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     public static Connection getConnection() throws SQLException {
         // Absolute path to your specific DB folder
-        String url = "jdbc:derby:C:/Users/Matt/Desktop/New folder/tube_db";
+        //String url = "jdbc:derby:C:/Users/Matt/Desktop/New folder/tube_db";
+        String dbPath = System.getProperty("user.home") + "/tube_db";
+        String url = "jdbc:derby:" + dbPath + ";create=true";
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return DriverManager.getConnection(url);
+        Connection connection = DriverManager.getConnection(url);
+        DatabaseSetup.initialiseIfNeeded(connection);
+        return connection;
+        //return DriverManager.getConnection(url);
     }
 }
